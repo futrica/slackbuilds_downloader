25b6e327c86192bab3911d00761a3e8d0e9c4eb6
futrica <jmfutrica@gmail.com>
2014-03-06 01:32:26 -0300

    compilando

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ dependencies = file.set_dep()
 ##print("Procurando arquivo ... ")
 ##print("Arquivo encontrado: ", file.name)
 ##print(" ")
+##print("fazendo download build")
+##f = subprocess.Popen(["wget", "-c",file.down_links[1]])
+##f.communicate()
+##name = file.name + ".tar.gz"
+##print("extraindo")
+##f = subprocess.Popen(["/bin/tar", "-xf", name])
+##f.communicate()
+##folder = file.name + "/"
+##print("fazendo download source")
+##z = subprocess.Popen(["wget", "-c","-P", folder, file.down_links[0]])
+##z.communicate()
+##print(" ")
+##print("dando permissão")
+##folder = file.name + "/"
+##build = file.name + ".SlackBuild"
+##p = subprocess.Popen(["chmod", "+x",folder, build])
+##p.communicate()
+
+##
 ##print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 ##
 ##print(" ")
@@ -27,28 +46,47 @@ dependencies = file.set_dep()
 ##for x in dependencies:
 ##    x.to_string()
 ##    print(" ")
-##
+##    
+
+
 ##print(" ")
 ##print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 ##print(" ")
+##
+##print("Instalando Dependências: ")
+##print(" ")
 
-print("Instalando Dependências: ")
-print(" ")
 i = len(dependencies) -1
-
 while (i >= 0):
-    print("Dependência: ", dependencies[i].name)
-    print("fazendo download dos arquivos..... ")
-    for k in range(2):
-        if k == 0:
-            p = subprocess.Popen(["wget", "-c","-P","depen",dependencies[i].down_links[k]])
-            if p.communicate():
-                print("Source ... Complete")
-        else:
-            p = subprocess.Popen(["wget", "-c","-P","depen",dependencies[i].down_links[k]])
-            if p.communicate():
-                print("Build ... Complete")
-    i -= 1
-    print(" ")
-    
+##    print("Dependência: ", dependencies[i].name)
+##    print("fazendo download da build..... ")
+##    p = subprocess.Popen(["wget", "-c",dependencies[i].down_links[1]])
+##    p.communicate()
+##    print("Complete")
+##    
+##    print("extraindo ..." , dependencies[i].name)
+##    name = dependencies[i].name + ".tar.gz"
+##    q = subprocess.Popen(["/bin/tar", "-xf", name])
+##    q.communicate()
+##    print("Complete")
+##
+##    print("fazendo download do source... ", dependencies[i].name)
+##    folder = dependencies[i].name + "/"
+##    p = subprocess.Popen(["wget", "-c","-P", folder, dependencies[i].down_links[0]])
+##    p.communicate()
+##    print("Complete")
+##
+##    print("dando permissões... ", dependencies[i].name)
+##    folder = dependencies[i].name + "/"
+##    build = dependencies[i].name + ".SlackBuild"
+##    p = subprocess.Popen(["chmod", "+x",folder, build])
+##    p.communicate()
+##    print("Complete")
 
+    print("compilando a build... ", dependencies[i].name)
+    folder = dependencies[i].name + "/"
+    build =  dependencies[i].name + ".SlackBuild"
+    p = subprocess.Popen(["/bin/sh" , build],cwd=folder)
+    p.communicate()
+    print("Complete")
+    i -= 1
